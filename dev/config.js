04b22dc7630f9bd1a7341acb4382364575feb53d
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-02-03 20:28:15 +0000

    refactor: cleaned up index.js

--- dev/config.js
+++ dev/config.js
@@ -3,6 +3,8 @@ export default {
 
   mongoPath: 'mongodb://localhost/tordoir-dev',
 
+  enableJwt: true,
+
   jwtSettings: {
     jwksUri: 'https://spip.eu.auth0.com/.well-known/jwks.json',
     audience: 'https://spip.spatialnetworkslab.com', // this looks like a URL but is just an identifier
