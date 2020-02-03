a59e40e2abba4e1946cd51d70fa4133b4489aa0e
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-02-03 20:16:21 +0000

    chore: removed yargs

--- dev/config.js
+++ dev/config.js
@@ -1,9 +1,11 @@
 export default {
+  port: 3002,
+
   mongoPath: 'mongodb://localhost/tordoir-dev',
 
   jwtSettings: {
     jwksUri: 'https://spip.eu.auth0.com/.well-known/jwks.json',
     audience: 'https://spip.spatialnetworkslab.com', // this looks like a URL but is just an identifier
-    issuer: `https://spip.eu.auth0.com/`
+    issuer: 'https://spip.eu.auth0.com/'
   }
 }
