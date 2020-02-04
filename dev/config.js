f9794a3e1b973d85a31f4a0d41b08b54161d9cdf
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-02-04 16:03:19 +0000

    refactor: cleanup setupApp

--- dev/config.js
+++ dev/config.js
@@ -3,10 +3,9 @@ import nodes from './dataset-descriptions/nodes'
 
 export default {
   port: 3002,
+  portR: 3005,
 
   mongoPath: 'mongodb://localhost/tordoir-dev',
-
-  enableJwt: true,
 
   jwtSettings: {
     jwksUri: 'https://spip.eu.auth0.com/.well-known/jwks.json',
