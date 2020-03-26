7c729df867d0e00e03010537accabe7137c37e98
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-03-26 15:42:30 +0100

    feat: added auth route

---
+++ src/routes/auth.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+export default function setupAuthRoute (app, auth) {
+  const router = express.Router()
+
+  router.route('/auth').get((req, res) => {
+    if (auth) {
+      res.send(auth.frontend)
+    }
+
+    if (!auth) {
+      res.send(null)
+    }
+  })
+
+  app.use('/api', router)
+}
