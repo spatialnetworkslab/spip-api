d9a861e4665291a93d859b1d32009f45f70616b1
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-03-26 18:03:50 +0100

    _

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,6 +4,9 @@ export default function setupAuthRoute (app, auth) {
   const router = express.Router()
 
   router.route('/auth').get((req, res) => {
+    console.log('HELLOOOOOOOOOOOO?')
+    console.log(auth)
+
     if (auth) {
       res.send(auth.frontend)
     }
