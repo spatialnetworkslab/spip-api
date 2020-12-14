7bc40b9e48fa33d79d7c2cd9cdd7baa4f92230fc
luucvanderzee <luuc_v_d_zee@hotmail.com>
2020-12-14 12:13:37 +0100

    feat: added cors library again (#123)

--- src/setup/setupApp.js
+++ src/setup/setupApp.js
@@ -6,6 +6,7 @@ import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import path from 'path'
 import history from 'connect-history-api-fallback'
+import cors from 'cors'
 
 import setupMongoose from './setupMongoose.js'
 import setupJwt from './setupJwt.js'
@@ -22,12 +23,7 @@ import configFrontend from '../routes/configFrontend.js'
 
 export default function setupApp (config) {
   const app = express()
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-  })
+  app.use(cors())
 
   app.use(helmet()) // helmet sets a number of security based middlewares
   // app.use(morgan('dev')) // morgan for logging
